d796ec4b5b24fce4bc4582c1cb2efefc86df2bb3
Ivan Roman <ivan.roman@color.com>
2022-12-06 14:56:02 -0600

    Adds faulty test

--- src/hello_test.py
+++ src/hello_test.py
@@ -5,9 +5,7 @@ from hello import add, substract
 class TestHello(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(1, 2), 4)
 
     def test_substract(self):
         self.assertEqual(substract(2, 1), 1)
-
-if __name__ == '__main__':
-    unittest.main()
