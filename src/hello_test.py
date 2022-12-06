73159f4ee8c8742ebae700a04b5a7b945967900c
Ivan Roman <ivan.roman@color.com>
2022-12-06 15:07:35 -0600

    Adds express server

--- src/hello_test.py
+++ src/hello_test.py
@@ -1,11 +1,16 @@
 import unittest
+import requests
 from hello import add, substract
 
 
 class TestHello(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(1, 2), 4)
 
     def test_substract(self):
         self.assertEqual(substract(2, 1), 1)
+
+    def test_server(self):
+        r = requests.get('http://localhost:3000')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.content, b'Hello World!')
